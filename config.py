fd8161ae13af020de016f72126814e664b613a0b
asgore-undertale <ahmaddraa2000@gmail.com>
2025-04-08 23:37:55 +0300

    fixing configs and reqs

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 
-db_dir = os.path.join(os.getcwd(), "databases")
-if not os.path.exists(db_dir):
-    os.makedirs(db_dir)
-
-# Configuration settings
+db_dir = os.path.join(os.getcwd(), 'databases')
 class Config:
     SECRET_KEY = 'PIO757UJK24356Lkh134nohvh34vfh124355'
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(db_dir, 'UniversityDb.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+POPULATION_SIZE = 20
+GENERATIONS_NUM = 100
+SELECTION_RATE = 0.2
+DUMP_RATE = 0.2
+ERASION_RATE = 0.2
